8dc4e5ed7f75059e18a97aba035f303f0484a958
angaratosurion <angarato.surion@gmail.com>
2022-04-10 16:48:45 +0300

    t
    starting making the app to returnr the exif data of the file
    getimages becomes static

--- WallpaperEngineSlideShowServer/Managers/WallpaperManager.cs
+++ WallpaperEngineSlideShowServer/Managers/WallpaperManager.cs
@@ -2,27 +2,33 @@
 using MetadataExtractor.Formats.Exif;
 using Microsoft.AspNetCore.Cors;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Metadata.Profiles.Exif;
+using WallpaperEngineSlideShowServer.Models;
 
 namespace WallpaperEngineSlideShowServer.Managers
 {
     [EnableCors("MyAllowSpecificOrigins")]
     public class WallpaperManager
     {
-
-        public List<string> GetImages()
+       static  List<string> Images;
+        public   WallpaperManager()
+        {
+            Images=GetImages();
+        }
+        public static  List<string> GetImages()
         {
             try
             {
                 List<string> ap = new List<string>();
                 string path = AppSettingsManager.GeWallPaperPath();
-                if (path != null && Directory.Exists(path))
+                if (path != null && System.IO.Directory.Exists(path))
                 {
                     var ext = AppSettingsManager.GetExtentions();
                     if ( ext == null)
                     {
                         ext = "*.*";
                     }
-                    var files = Directory.GetFiles(path,ext);
+                    var files = System.IO.Directory.GetFiles(path,ext);
                     if (files != null)
                     {
                         //foreach(var file in files)
@@ -54,7 +60,11 @@ namespace WallpaperEngineSlideShowServer.Managers
             {
                 string ap = null;
                 int i = 0;
-                var images = this.GetImages();
+                if ( Images== null)
+                {
+                    Images = GetImages();
+                }
+                var images = Images; ;
                 if (images != null)
                 {
                     Random random = new Random();
@@ -85,7 +95,11 @@ namespace WallpaperEngineSlideShowServer.Managers
                 string ap = null;
                 int i = 0;
                 string timgpath,tap=null;
-                var images = this.GetImages();
+                if ( Images== null)
+                {
+                    Images= GetImages();
+                }
+                var images = GetImages();
                 if (images != null)
                 {
                     Random random = new Random();
@@ -95,7 +109,7 @@ namespace WallpaperEngineSlideShowServer.Managers
                         i = images.Count - 1;
                     }
                   timgpath = images[i];
-
+                    
                     Console.WriteLine(timgpath);
                     if (timgpath != null && File.Exists(timgpath))
                     {
@@ -168,22 +182,53 @@ namespace WallpaperEngineSlideShowServer.Managers
         }
 
 
-        public ExifIfd0Directory GetMetadata(string path)
+        public ExifModel GetMetadata(string path)
         {
             try
             {
-                ExifIfd0Directory ap = null; ;
+                ExifModel ap = null; ;
                 if (path != null && File.Exists(path))
                 {
-                    List<MetadataExtractor.Directory> directories = ImageMetadataReader.ReadMetadata(path).ToList();
-                    if( directories!=null)
+                    using (Image image = Image.Load(path))
                     {
-                        var   exif = directories.OfType<ExifIfd0Directory>().FirstOrDefault();
-                       
+                        var exif = image.Metadata.ExifProfile ; 
+                        if (exif != null)
+                        {
+                             ap= new ExifModel();
+                               var met= exif.GetValue(ExifTag.Acceleration);
+                            ap.Acceleration = this.RationalToString((int) exif.GetValue(ExifTag.Acceleration).Value.Numerator, 
+                                (int)exif.GetValue(ExifTag.Acceleration).Value.Denominator);
+                            ap.AmbientTemperature = this.RationalToString(exif.GetValue(ExifTag.AmbientTemperature).Value.Numerator,
+                                exif.GetValue(ExifTag.AmbientTemperature).Value.Denominator);
+                            ap.Exposure_Time= this.RationalToString((int)exif.GetValue(ExifTag.ExposureTime).Value
+                                .Numerator, (int)exif.GetValue(ExifTag.ExposureTime).Value.Denominator);
+                            ap.F_number=this.RationalToString((int)exif.GetValue(ExifTag.FNumber).Value.Numerator, 
+                                (int)exif.GetValue(ExifTag.FNumber).Value.Denominator) ;
+                            ap.Exposure_Program = exif.GetValue(ExifTag.ExposureProgram).Value;
+                            ap.Model=exif.GetValue(ExifTag.Model).Value;
+                            ap.Orientation= exif.GetValue(ExifTag.Orientation).Value;
+                            ap.Resolution_Unit= exif.GetValue(ExifTag.ResolutionUnit).Value;
+                            ap.X_resolution= exif.GetValue(ExifTag.XResolution).Value.ToDouble();
+                            ap.Y_resolution= exif.GetValue(ExifTag.YResolution).Value.ToDouble();
+                            ap.YCbCr_Positioning= exif.GetValue(ExifTag.YCbCrPositioning).Value;
+                           ap.DateTaken= exif.GetValue(ExifTag.DateTimeOriginal).Value;
+                            ap.Brightness= this.RationalToString(exif.GetValue(ExifTag.BrightnessValue).Value.Numerator,
+                                exif.GetValue(ExifTag.BrightnessValue).Value.Denominator);
+                            ap.GPSDateStamp= exif.GetValue(ExifTag.GPSDateStamp).Value;
+                            ap.GPSAltitude=RationalToString((int)exif.GetValue(ExifTag.GPSAltitude).Value.Numerator,
+                                (int)exif.GetValue(ExifTag.GPSAltitude).Value.Denominator);
+                            ap.GPSLongitude = RationalToArStringAR(exif.GetValue(ExifTag.GPSLongitude).Value);
+                            ap.GPSLatitude=RationalToArStringAR(exif.GetValue(ExifTag.GPSLatitude).Value);
+                            ap.ImageWidth = exif.GetValue(ExifTag.ImageWidth).Value.ToString();
+                            ap.ImageLength = exif.GetValue(ExifTag.ImageLength).Value.ToString();
+
+
+
+                        }
+
                     }
-                        
                 }
-                return ap;
+                    return ap;
 
             }
             catch (Exception ex)
@@ -196,16 +241,63 @@ namespace WallpaperEngineSlideShowServer.Managers
         {
             String base64String = null;
 
-            Image image =  Image.Load(path);
+            using (Image image = Image.Load(path)) 
+            {
 
-            
                 
-                    var imgformat=Image.DetectFormat(path);
-                base64String= image.ToBase64String(imgformat);
+
+                var imgformat = Image.DetectFormat(path);
+                base64String = image.ToBase64String(imgformat);
+            }
                     
                     return base64String;
                
             }
+        public string RationalToString(int numerator,int denominator)
+        {
+            try
+            { 
+                return String.Format("{0}/{1}", numerator, denominator);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString()); return null;
+            }
         }
+        public string RationalToArStringAR(SixLabors.ImageSharp.Rational[] rationals)
+        {
+            try
+            {
+                string ap = null;
+
+                if( rationals != null )
+                {
+                    foreach( SixLabors.ImageSharp.Rational r in rationals )
+                    {
+                        ap += " "+ this.RationalToString((int)r.Numerator, (int)r.Denominator);
+                    }
+                }
+
+
+                return ap;
+               
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString()); return null;
+            }
+        }
+
+        private void RationalToString(int numerator, uint denominator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+ 
+
     }
 
--- WallpaperEngineSlideShowServer/Models/ExifModel.cs
+++ WallpaperEngineSlideShowServer/Models/ExifModel.cs
@@ -2,18 +2,40 @@
 {
     public class ExifModel
     {
+
+
+        public String Acceleration { get; set; }
+        public String AmbientTemperature { get; set; }
+
+
         public string Manufacturer { get; set; }
         public string Model { get; set; }
+        public ushort Orientation { get; set; }
         public string Orientation_Rotation { get; set; }
-        public DateTime DateAndTime { get; set; }
-        public string YCbCr_Positioning{ get; set; }
-        public string Compression { get; set; }
-        public float X_resolution { get; set; }
-        public float Y_resolution { get; set; }
-        public string Resolution_Unit { get; set; }
+        public double X_resolution { get; set; }
+        public double Y_resolution { get; set; }
+        public ushort Resolution_Unit { get; set; }
         public string Exposure_Time { get; set; }
-        public string F_number{ get; set; }
-        public string Exposure_Program { get; set; }
+        public string F_number { get; set; }
+        public ushort Exposure_Program { get; set; }
+        public ushort YCbCr_Positioning { get; set; }
+        public string DateTaken { get; set; }
+        public string Brightness { get; set; }
+        public string GPSDateStamp { get; set; }
+        public string GPSAltitude { get; set; }
+        public string GPSLatitude { get; set; }
+        public string GPSLongitude { get; set; }
+        public string  ImageWidth { get; set; }
+        public string ImageLength { get; set; }
+
+
+
+
+        /* public DateTime DateAndTime { get; set; }
+        
+         public string Compression { get; set; }
+
+         */
 
 
 
